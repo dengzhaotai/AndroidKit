778f7691f4967119f54002d8eed8113b74d3a05d
dengzhaotai <dengzhaotai@aliyun.com>
2017-10-20 11:37:56 +0800

    使用dataBinding数据绑定

--- app/src/main/java/com/dzt/kit/activity/AboutActivity.java
+++ app/src/main/java/com/dzt/kit/activity/AboutActivity.java
@@ -1,10 +1,13 @@
 package com.dzt.kit.activity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import com.dzt.androidkit.activity.FrameActivity;
 import com.dzt.kit.R;
 import com.dzt.kit.databinding.ActivityAboutBinding;
+import com.dzt.kit.model.MenuItem;
 
 public class AboutActivity extends FrameActivity<ActivityAboutBinding> {
 
@@ -18,6 +21,9 @@ public class AboutActivity extends FrameActivity<ActivityAboutBinding> {
 	protected void initWidgets() {
 		showContentView();
 		setTitle("关于");
+		//Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+		bindingView.setMenu(new MenuItem(R.mipmap.ic_launcher, "图标"));
+		bindingView.setResId(R.mipmap.ic_launcher);
 	}
 
 	@Override
--- app/src/main/java/com/dzt/kit/model/MenuItem.java
+++ app/src/main/java/com/dzt/kit/model/MenuItem.java
@@ -1,17 +1,15 @@
 package com.dzt.kit.model;
 
-import android.graphics.Bitmap;
-
 /**
  * Created by M02323 on 2017/10/18.
  */
 
 public class MenuItem {
-	public Bitmap bitmap;
+	public int  resId;
 	public String title;
 
-	public MenuItem(Bitmap bitmap, String title) {
-		this.bitmap = bitmap;
+	public MenuItem(int resId, String title) {
+		this.resId = resId;
 		this.title = title;
 	}
 }
